90c6b2298b3eccf420c5ae97f56079b4e7ccfbea
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-10-30 17:24:56 +0300

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,24 @@
-from models import Dog
+from models import Dog, Base
 
-def create_table(base):
-    pass
+def create_table(Base, engine):
+    Base.metadata.create_all(bind=engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
